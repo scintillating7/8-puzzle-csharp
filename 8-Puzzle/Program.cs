0ceac2f76e7bbdffb6ea311adf4227a2a0d89662
tkrizzle2000 <queenvkraft@gmail.com>
2017-01-11 21:30:12 -0600

    Iterative Deepening is working, except fix the nodes visited thing. Uniform Cost began

--- 8-Puzzle/Program.cs
+++ 8-Puzzle/Program.cs
@@ -8,31 +8,38 @@ namespace _8_Puzzle
 {
     class Program
     {
+        private static readonly int MAX_DEPTH = 5;
+        private static readonly string EASY = "134862705";
+        private static readonly string MEDIUM = "281043765";
+
         static void Main(string[] args)
         {
-            string defaultArgs = "281043765";//"134862705";
+            string defaultArgs = EASY;
             Board initialBoard = Board.CreateNew(defaultArgs);
-            
-    
+            Node rootNode = new Node(initialBoard, null);
+
             Console.WriteLine("Initial State: " + initialBoard.ToString());
 
             if (initialBoard.isInEndState() == false) {
-                
-                doBreadthFirstSearch(initialBoard);
+              
+                doBreadthFirstSearch(rootNode);
+                doDepthFirstSearch(rootNode);
 
-                doDepthFirstSearch(initialBoard);
+                //harder stuff
+                //TODO: fix nodes visited for this thing
+                
+                doIterativeDeepening(rootNode);
             }
         }
 
-        static void doBreadthFirstSearch(Board board)
+        static void doBreadthFirstSearch(Node rootNode)
         {
             Console.WriteLine("Beginning breadth first search");
 
             Queue<Node> q = new Queue<Node>();
-            Node newNode = new Node(board, null);
-            q.Enqueue(newNode);
+            q.Enqueue(rootNode);
 
-            HashSet<int> alreadySeen = new HashSet<int> { board.Id };
+            HashSet<int> alreadySeen = new HashSet<int> { rootNode.board.Id };
 
             int iterations = 0;
             while (q.Count > 0)
@@ -68,13 +75,12 @@ namespace _8_Puzzle
             }
         }
 
-        static void doDepthFirstSearch(Board board)
+        static void doDepthFirstSearch(Node rootNode)
         {
             Console.WriteLine("Beginning depth first search");
 
             Stack<Node> s = new Stack<Node>();
-            HashSet<int> alreadySeen = new HashSet<int> { board.Id };
-            Node rootNode = new Node(board, null);
+            HashSet<int> alreadySeen = new HashSet<int> { rootNode.board.Id };
             s.Push(rootNode);
 
             int iterations = 0;
@@ -108,13 +114,67 @@ namespace _8_Puzzle
             }         
         }
 
-        static void doBreadthFirstSearchWithHeuristic(Board board)
+        static void doIterativeDeepening(Node rootNode)
+        {
+            Console.WriteLine("Beginning iterative deepening");
+
+            for (int depth = 0; depth < Int32.MaxValue; depth++)
+            {
+                Node endNode = doDepthLimitedDFS(rootNode, depth);
+                if (endNode != null)
+                {
+                    //we're done
+                    return;
+                }                
+            }
+        }
+
+        static Node doDepthLimitedDFS(Node currentNode, 
+                                         int depth)
+        {
+
+            Board currentBoard = currentNode.board;
+            if (currentBoard.isInEndState())
+            {
+                //found endState
+                //use this opportunity to write path to console,
+                //as it will be rewinded by recursive call.
+                handleEndState(currentNode, currentNode.depth);
+                return currentNode;
+            }
+
+            if (depth <= 0)
+            {
+                return null;
+            }
+
+            List<Board> children = currentBoard.GenerateNextStates();
+
+            foreach (Board child in children)
+            {
+                Node childNode = new Node(child,
+                                        currentNode,
+                                        currentNode.cost + child.TileMoved,
+                                        currentNode.depth + 1);
+
+                if (doDepthLimitedDFS(childNode, depth - 1) != null)
+                {
+                    return childNode;
+                }
+            }
+
+            return null;
+        }
+
+
+        //TODO: make sure the repeated state checking has both
+        //isExpanded, and cost is less, two things
+        static void doUniformCost(Node rootNode)
         {
             C5.IntervalHeap<Node> q = new C5.IntervalHeap<Node>();
-            Node newNode = new Node(board, null);
-            q.Add(newNode);
+            q.Add(rootNode);
 
-            HashSet<int> alreadySeen = new HashSet<int> { board.Id };
+            HashSet<int> alreadySeen = new HashSet<int> { rootNode.board.Id };
 
             int iterations = 1;
             while (q.Count > 0)
@@ -124,41 +184,38 @@ namespace _8_Puzzle
 
                 Console.WriteLine("Current board being considered: " + currentBoard.ToString());
 
+                Console.WriteLine("Child no. " + iterations);
 
-                if (currentBoard != null)
+                if (currentBoard.isInEndState())
                 {
-                    Console.WriteLine("Child no. " + iterations);
-
-                    if (currentBoard.isInEndState())
-                    {
-                        //handleEndState(currentBoard, iterations);
-                        return;
-                    }
-
-                    List<Board> children = currentBoard.GenerateNextStates();
-
-                    foreach (Board child in children)
-                    {
-                        if (!alreadySeen.Contains(child.Id))
-                        {
-                            //q.Enqueue(child);
-                            Node childNode = new Node(child,
-                                                      currentNode,
-                                                      currentNode.cost + child.TileMoved,
-                                                      currentNode.depth + 1);
-
-                            writeBoardInfo(childNode);
-
-                            q.Add(childNode);
-                            alreadySeen.Add(child.Id);
-                        }
-                    }
-
-                    //Console.WriteLine("There are now: " + q.Count + " nodes in the list.");
-                    iterations += 1;
+                    handleEndState(currentNode, iterations);
+                    return;
                 }
+
+                List<Board> children = currentBoard.GenerateNextStates();
+
+                foreach (Board child in children)
+                {
+                    if (!alreadySeen.Contains(child.Id))
+                    {
+
+                        Node childNode = new Node(child,
+                                                    currentNode,
+                                                    currentNode.cost + child.TileMoved,
+                                                    currentNode.depth + 1);
+
+                        writeBoardInfo(childNode);
+
+                        q.Add(childNode);
+                        alreadySeen.Add(child.Id);
+                    }
+                }
+
+                iterations += 1;
+
             }
         }
+
 
         static void handleEndState(Node endBoardNode,
                                    int iterations)
